4ac4d88754d68c3962ee51758275661f8a33258e
ntp612k4 <114236234+ntp612k4@users.noreply.github.com>
2025-05-26 18:36:08 +0700

    refactor code

--- MotorModule.py
+++ MotorModule.py
@@ -4,20 +4,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 class Motor():
-    def __init__(self, EN, IN1, IN2, ENB, IN1B, IN2B):
-        self.EN = EN
+    def __init__(self, ENA, IN1, IN2, ENB, IN3, IN4):
+        self.ENA = ENA
         self.IN1 = IN1
         self.IN2 = IN2
         self.ENB = ENB
-        self.IN1B = IN1B
-        self.IN2B = IN2B
-        GPIO.setup(self.EN, GPIO.OUT)
+        self.IN3 = IN3
+        self.IN4 = IN4
+        GPIO.setup(self.ENA, GPIO.OUT)
         GPIO.setup(self.IN1, GPIO.OUT)
         GPIO.setup(self.IN2, GPIO.OUT)
         GPIO.setup(self.ENB, GPIO.OUT)
-        GPIO.setup(self.IN1B, GPIO.OUT)
-        GPIO.setup(self.IN2B, GPIO.OUT)
-        self.pwmA = GPIO.PWM(self.EN, 100)
+        GPIO.setup(self.IN3, GPIO.OUT)
+        GPIO.setup(self.IN4, GPIO.OUT)
+        self.pwmA = GPIO.PWM(self.ENA, 100)
         self.pwmA.start(0)
         self.pwmB = GPIO.PWM(self.ENB, 100)
         self.pwmB.start(0)
@@ -49,11 +49,11 @@ class Motor():
             GPIO.output(self.IN2, GPIO.HIGH)
             
         if rightSpeed > 0:
-            GPIO.output(self.IN1B, GPIO.HIGH)
-            GPIO.output(self.IN2B, GPIO.LOW)
+            GPIO.output(self.IN3, GPIO.HIGH)
+            GPIO.output(self.IN4, GPIO.LOW)
         elif rightSpeed < 0:
-            GPIO.output(self.IN1B, GPIO.LOW)
-            GPIO.output(self.IN2B, GPIO.HIGH)
+            GPIO.output(self.IN3, GPIO.LOW)
+            GPIO.output(self.IN4, GPIO.HIGH)
         sleep(time)
         
     def stop(self, time=0):
@@ -61,12 +61,17 @@ class Motor():
         GPIO.output(self.IN1, GPIO.LOW)
         GPIO.output(self.IN2, GPIO.LOW)
         self.pwmB.ChangeDutyCycle(0)
-        GPIO.output(self.IN1B, GPIO.LOW)
-        GPIO.output(self.IN2B, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.LOW)
         sleep(time)
     
     def cleanup(self):
         self.pwmA.stop()
         self.pwmB.stop()
         GPIO.cleanup()
-         
+
+if __name__ == '__main__':
+    motor= Motor(27, 17, 18, 24, 22, 23)
+    while True:
+        motor.move(speed=0.2, time=0.2)
+        print("Di")
--- RobotMain.py
+++ RobotMain.py
@@ -1,5 +1,7 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 import torch
-# import MotorModule as Motor
+import MotorModule as Motor
 import WebcamModule as Webcam
 import cv2
 
@@ -12,19 +14,19 @@ model = torch.hub.load(
     verbose=True,
     source='local'
 )
-# motor = Motor.Motor(2, 3, 4, 17, 22, 27)
+motor = Motor.Motor(27, 17, 18, 24, 22, 23)
 
-def preProcess(img):
-    # img = img[54:120, :, :]
-    # img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    # img = cv2.GaussianBlur(img, (3, 3), 0)
-    # img = cv2.resize(img, (200, 66))
-    # img = img / 255
-    img = cv2.resize(img, (320, 240))
-    return img
+# def preProcess(img):
+#     # img = img[54:120, :, :]
+#     # img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+#     # img = cv2.GaussianBlur(img, (3, 3), 0)
+#     # img = cv2.resize(img, (200, 66))
+#     # img = img / 255
+#     img = cv2.resize(img, (320, 240))
+#     return img
 
 while True:
-    img = Webcam.getImg(True, size=[320, 240])
+    img = Webcam.getImg(False, size=[320, 320])
     # img = preProcess(img)
     
     if img is None:
@@ -39,18 +41,19 @@ while True:
         if conf > 0.5:
             target_detected = True
             print(f"Phát hiện: {class_name}, độ tin cậy: {conf:.2f}")
-            if class_name == "Cấm Dừng & Cấm Đỗ":
-                print("Phát hiện biển báo Cấm Dừng & Cấm Đỗ")
-                # motor.stop(time=0.1)
-            elif class_name == "Cấm Dừng":
-                print("Phát hiện biển báo Cấm Dừng")
-                # motor.stop(time=0.1)
-            elif class_name == "Cấm Đỗ":
-                print("Phát hiện biển báo Cấm Đỗ")
-                # motor.stop(time=0.1)
-            break
+            # if class_name == "Cấm Dừng & Cấm Đỗ":
+            #     print("Phát hiện biển báo Cấm Dừng & Cấm Đỗ")
+            #     motor.move(speed=0.2, turn=0)
+            # elif class_name == "Cấm Dừng":
+            #     print("Phát hiện biển báo Cấm Dừng")
+            #     motor.move(speed=0.2, turn=0)
+            # elif class_name == "Cấm Đỗ":
+            #     print("Phát hiện biển báo Cấm Đỗ")
+            #     motor.move(speed=0.2, turn=0)
+            # break
+            motor.move(speed=0.2, turn=0)
 
     if not target_detected:
-        # motor.move(speed=1, turn=0, time=0.1)
+        motor.stop()
         # print("Không phát hiện đối tượng, tiếp tục di chuyển")
         continue
